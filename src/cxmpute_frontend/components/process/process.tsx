ffa8ebbde0404bd8ce0a052194b3a8b20743df79
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-02-22 16:26:46 -0600

    added process component

--- src/cxmpute_frontend/components/process/process.tsx
+++ src/cxmpute_frontend/components/process/process.tsx
@@ -4,7 +4,45 @@ import * as React from 'react';
 const Process = () => {
     return (
         <div className={styles.process}>
-            <p className={styles.processText}>Process</p>
+            <h1 className={styles.processText}>How It Works</h1>
+            <div className={styles.processContainer}>
+                <h1>For Consumers</h1>
+                <div className={styles.processStep}>
+                    <h2>Step 1</h2>
+                    <p>Create an account and load credits. Cxmpute accepts fiat and cryptocurrencies.</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 2</h2>
+                    <p>Read the docs to learn how to use Cxmpute.</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 3</h2>
+                    <p>Start developing and innovating!</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 4 (Optional)</h2>
+                    <p>Buy CXPT tokens and earn rewards and a share of protocol fees.</p>
+                </div>
+            </div>
+            <div className={styles.processContainer}>
+                <h1>For Providers</h1>
+                <div className={styles.processStep}>
+                    <h2>Step 1</h2>
+                    <p>Register on Cxmpute.xyz</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 2</h2>
+                    <p>Download the Provider Desktop software.</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 3</h2>
+                    <p>Complete system diagnostics and provider onboarding.</p>
+                </div>
+                <div className={styles.processStep}>
+                    <h2>Step 4</h2>
+                    <p>Stake CXPT tokens and start earning from your provisions!</p>
+                </div>
+            </div>
         </div>
     );
 };

394644070302c2d8c21c56f9b214196afa2622cc
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-02-22 19:38:34 -0600

    added footer. finished homepage revamp

--- src/cxmpute_frontend/components/footer/footer.tsx
+++ src/cxmpute_frontend/components/footer/footer.tsx
@@ -11,58 +11,63 @@ export default function Footer({ toggleDashboard }: FooterProps) {
             <div className={styles.footerText}>
                 <h1 className={styles.footerText2}>CXMPUTE</h1>
             </div>
-            <ul className={styles.footerLink}>
-                <li
+            <div className={styles.footerButtons}>
+                <ul
+                    className={styles.footerLink}
                     onClick={() => {
-                        window.open('https://x.com/cxmpute', '_blank');
+                        window.location.href = 'https://github.com/cxmpute/cxmpute';
                     }}
-                >Twitter</li>
-                <li
-                    onClick={() => {
-                        window.open('https://discord.com/invite/CJGA7B2zKT', '_blank');
-                    }}
-                >Discord</li>
-                <li
-                    onClick={() => {
-                        window.open('https://www.youtube.com/@cxmputenetwork/', '_blank');
-                    }}
-                >Youtube</li>
-                <li
-                    onClick={() => {
-                        window.open('https://github.com/unxversal', '_blank');
-                    }}
-                >Github</li>
-            </ul>
-            <ul className={styles.footerLink}>
-                <li>Home</li>
-                <li
-                    onClick={() => {
-                        window.open('https://www.joshuaokolo.xyz/blog/cxmpute101', '_blank');
-                    }}
-                >Cxmpute in 90 seconds</li>
-                <li
-                    onClick={() => {
-                        window.open('https://drive.google.com/file/d/1JN7-61fcTdFn3OM5Z-pAXi1VEUmuLX8p/view?usp=sharing', '_blank');
-                    }}
-                >Whitepaper</li>
-                <li
-                    onClick={() => {
-                        window.open('https://unxversal.github.io/', '_blank');
-                    }}
-                >Docs</li>
-                <li
-                    onClick={toggleDashboard}
-                >Launch Demo App</li>
-                <li>Get Updates</li>
-            </ul>
-            <ul
-                className={styles.footerLink}
-                onClick={() => {
-                    window.location.href = 'https://github.com/cxmpute/cxmpute';
-                }}
-            >
-                <li>GitHub</li>
-            </ul>
+                >
+                    <li>GET STARTED AS A PROVIDER</li>
+                    <li>GET STARTED AS A USER</li>
+                    <li>GET UPDATES</li>
+                </ul>
+                <ul className={styles.footerLink}>
+                    <li
+                        onClick={() => {
+                            window.open('https://x.com/cxmpute', '_blank');
+                        }}
+                    >TWITTER</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://discord.com/invite/CJGA7B2zKT', '_blank');
+                        }}
+                    >DISCORD</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://www.youtube.com/@cxmputenetwork/', '_blank');
+                        }}
+                    >YOUTUBE</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://github.com/unxversal', '_blank');
+                        }}
+                    >GITHUB</li>
+                </ul>
+                <ul className={styles.footerLink}>
+                    <li>HOME</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://www.joshuaokolo.xyz/blog/cxmpute101', '_blank');
+                        }}
+                    >CXMPUTE 101</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://drive.google.com/file/d/1JN7-61fcTdFn3OM5Z-pAXi1VEUmuLX8p/view?usp=sharing', '_blank');
+                        }}
+                    >WHITEPAPER</li>
+                    <li
+                        onClick={() => {
+                            window.open('https://unxversal.github.io/', '_blank');
+                        }}
+                    >DOCS</li>
+                    {/* <li
+                        onClick={toggleDashboard}
+                    >LAUNCH DEMO APP</li> */}
+                
+                </ul>
+            </div>
+
         </div>
     );
 }
